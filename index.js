d0c33583a8e7c56216f73f10afe569ece45ee954
Robert Bubnik <rbubnik@gmail.com>
2024-12-06 17:42:18 -0500

    passing all tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown (number) {
+    
+    for (let i= number; i >-1; i--) {
+    console.log(i)
+    
+    }
+}
+
+function writeCards (array) {
+    let newArray = []
+    
+    for (let name in array) {
+       newArray.push(`Thank you, ${array[name]}, for the wonderful surprise gift!`)
+    }
+    return newArray
+}
